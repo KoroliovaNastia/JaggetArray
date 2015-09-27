0ac1c0cebd6c5ad9aa9c84480bcb400cf6eff88e
KoroliovaNastia <K.Nastia.san@mail.ru>
2015-09-27 20:26:50 +0300

    JagArr

---
+++ ConsoleJaggetArray/ConsoleJaggetArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicForArray;
+
+namespace ConsoleJaggetArray
+{
+    class Program
+    {
+        static void Show(int[][] a) 
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a[i].Length; j++)
+                {
+                    Console.Write(a[i][j]);
+                    Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray = new int[3][];
+            jaggedArray[0] = new int[] { 1, 31, 5, 7 };
+            jaggedArray[1] = new int[] { 2, 14 };
+            jaggedArray[2] = new int[] { 11, 3, 9 };
+
+            //Logic.SortMaxElem(jaggedArray);
+            //Logic.SortMinElem(jaggedArray);
+            Logic.SortMaxSumm(jaggedArray);
+            Show(jaggedArray);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleJaggetArray/Project1/Logic.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicForArray
+{
+    public static class Logic
+    {
+        public static void SortMinElem(int[][] m)
+        {
+            for (int i = 0; i < m.Length; i++)
+            {
+                for (int j = i + 1; j < m.Length; j++)
+                {
+                    if (Logic.MinElem(m[i]) > Logic.MinElem(m[j])) Logic.Swap(ref m[0], ref m[1]);
+                }
+            }
+        }
+        public static void SortMaxElem(int[][] m)
+        {
+            for (int i = 0; i < m.Length; i++)
+            {
+                for (int j = i + 1; j < m.Length; j++)
+                {
+                    if (Logic.MaxElem(m[i]) > Logic.MaxElem(m[j])) Logic.Swap(ref m[i], ref m[j]);
+                }
+            }
+        }
+
+        public static void SortMaxSumm(int[][] m)
+        {
+            for (int i = 0; i < m.Length; i++)
+            {
+                for (int j = i + 1; j < m.Length; j++)
+                {
+                    if (Logic.Summ(m[i]) < Logic.Summ(m[j])) Logic.Swap(ref m[i], ref m[j]);
+                }
+            }
+        }
+        private static int MaxElem(int[] mass)
+        {
+            int max = mass[0];
+            for (int i = 1; i < mass.Length; i++)
+            {
+                if (max < mass[i]) max = mass[i];
+            }
+            return max;
+        }
+        private static int MinElem(int[] mass)
+        {
+            int min = mass[0];
+            for (int i = 1; i < mass.Length; i++)
+            {
+                if (min > mass[i]) min = mass[i];
+            }
+            return min;
+        }
+        private static int Summ(int[] mass)
+        {
+            int sum = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                sum += mass[i];
+            }
+            return sum;
+        }
+
+        static void Swap(ref int[] a, ref int[] b)
+        {
+            int[] temp = a;
+            a = b;
+            b = temp;
+        
+        }
+
+    }
+}
+
